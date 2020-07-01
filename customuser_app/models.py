26eeadf9898ee22479a0926da79fda871a77a9b8
luisfff29 <luisfff29@gmail.com>
2020-07-01 10:41:50 -0500

    extend custom user field & command superuser ask for age

--- customuser_app/models.py
+++ customuser_app/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class MyUser(AbstractUser):
-    homepage = models.URLField()
+    homepage = models.URLField(null=True, blank=True)
     display_name = models.CharField(max_length=128, null=True, blank=True)
-    age = models.IntegerField()
+    age = models.IntegerField(null=True, blank=True)
+
+    # https://stackoverflow.com/questions/40396049/cannot-create-super-user-in-django
+    REQUIRED_FIELDS = ['age']
